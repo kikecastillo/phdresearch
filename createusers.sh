c32b0f3e11008b20240cb8c6e7ad5d9c3b3215b9
root <root@kaikal.com>
2016-09-30 11:49:14 +0200

    changes in recorder and script for users

--- createusers.sh
+++ createusers.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 NUMBER_OF_USERS=$1
-COUNTER=1982
-for i in `seq 1 $NUMBER_OF_USERS`; do
+OFFSET=$2
+COUNTER=$(($2 + 1982))
+for i in `seq $OFFSET $NUMBER_OF_USERS`; do
 	drush ucrt "user$i" --password="$COUNTER" --mail="user$i@test.com"
 	let COUNTER+=1
 done
